efff703d93629e237613d7d3dd7f3c08189ec3f0
Cara Franco <xbbkbnv@eagleinvsys.com>
2015-11-23 22:36:11 -0500

    add-a-controller

---
+++ myApp.js
@@ -0,0 +1,14 @@
+angular.module('myApp', [])
+    .controller('MyCtrl', function($scope) {
+
+    $scope.maleName='Male name';
+    $scope.dirtyTask='Dirty task';
+    $scope.obnoxiousCelebrity='Obnoxious celebrity';
+    $scope.jobTitle='Job title';
+    $scope.celebrity='Celebrity';
+    $scope.hugeNumber='Huge number';
+    $scope.tediousTask='Tedious task';
+    $scope.uselessSkill='Useless skill';
+    $scope.adjective='Adjective';
+
+});
