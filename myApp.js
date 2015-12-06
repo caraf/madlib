9fad385f0411f001d5e10bd496b3aa8cc9ed3515
Cara Franco <xbbkbnv@eagleinvsys.com>
2015-12-06 18:06:12 -0500

    New changes

--- myApp.js
+++ myApp.js
@@ -1,14 +1,59 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngMessages'])
     .controller('MyCtrl', function($scope) {
 
-    $scope.maleName='Male name';
-    $scope.dirtyTask='Dirty task';
-    $scope.obnoxiousCelebrity='Obnoxious celebrity';
-    $scope.jobTitle='Job title';
-    $scope.celebrity='Celebrity';
-    $scope.hugeNumber='Huge number';
-    $scope.tediousTask='Tedious task';
-    $scope.uselessSkill='Useless skill';
-    $scope.adjective='Adjective';
+
+    $scope.data = [];
+
+    $scope.submit = function() {
+        console.log($scope.initForm);
+
+      
+       if  ($scope.initForm.maleName.$dirty &&
+           $scope.initForm.dirtyTask.$dirty &&
+           $scope.initForm.obnoxiousCelebrity.$dirty &&
+           $scope.initForm.jobTitle.$dirty &&
+           $scope.initForm.celebrity.$dirty &&
+           $scope.initForm.hugeNumber.$dirty &&
+           $scope.initForm.tediousTask.$dirty &&
+           $scope.initForm.uselessSkill.$dirty &&
+           $scope.initForm.adjective.$dirty)
+        {
+            $scope.allValsChange = true;
+        }
+
+        console.log($scope.allValsChange);
+
+        if ($scope.initForm.$valid && $scope.allValsChange)
+        {
+           $scope.initView = false;
+         
+        }
+
+    };
+
+
+    $scope.initValues = function() {
+        $scope.allValsChange = false;
+        $scope.initView=true;
+        $scope.data.maleName='Male name';
+        $scope.data.dirtyTask='Dirty task';
+        $scope.data.obnoxiousCelebrity='Obnoxious celebrity';
+        $scope.data.jobTitle='Job title';
+        $scope.data.celebrity='Celebrity';
+        $scope.data.hugeNumber='Huge number';
+        $scope.data.tediousTask='Tedious task';
+        $scope.data.uselessSkill='Useless skill';
+        $scope.data.adjective='Adjective';
+
+    };
+
+    $scope.initValues();
+
+    $scope.reset = function() {
+       $scope.initValues();
+       $scope.initForm.$submitted = false;
+       $scope.initForm.$setPristine();       
+
+    };
 
 });
